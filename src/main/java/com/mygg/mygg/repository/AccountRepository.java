57b79194d378376995ae560d3a73590555da5f06
S0I3R0I3 <71364866+S0I3R0I3@users.noreply.github.com>
2021-05-14 16:47:45 +0900

    Login & Join Page

--- src/main/java/com/mygg/mygg/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mygg.mygg.repository;
-
-import com.mygg.mygg.domain.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
