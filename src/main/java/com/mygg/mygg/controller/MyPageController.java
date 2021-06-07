245340cb82dd31c3c24aeb3324e1d3e91523f349
ChanWoo Park <diqksk@naver.com>
2021-06-07 10:43:25 +0900

    header 분리

--- src/main/java/com/mygg/mygg/controller/MyPageController.java
+++ src/main/java/com/mygg/mygg/controller/MyPageController.java
@@ -1,8 +1,10 @@
+package com.mygg.mygg.controller;
 
 import com.mygg.mygg.dto.MemberDTO;
 import com.mygg.mygg.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
